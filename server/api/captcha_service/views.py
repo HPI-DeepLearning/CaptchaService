602f4a887098522a366146834716fad481707abb
slayerpart <marvin.gorecki@student.hpi.uni-potsdam.de>
2016-12-01 16:59:50 +0100

    add basic endpoint functionality

--- server/api/captcha_service/views.py
+++ server/api/captcha_service/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import CaptchaToken
+from random import randint
 
 def request(request):
-    return render(request, "api_app/request.html", {})
+    if request.method == 'GET':
+        count = CaptchaToken.objects.all().__len__()
+        first_captcha, second_captcha = randint(1, count), randint(1, count)
+        first_url = CaptchaToken.objects.get(pk=first_captcha).file.url
+        second_url = CaptchaToken.objects.get(pk=second_captcha).file.url
+        return JsonResponse({'first_url': first_url,
+                             'second_url': second_url})
 
 def validate(request):
     return render(request, "api_app/validate.html", {})
 
 def renew(request):
     return render(request, "api_app/renew.html", {})
+
+
+def get_random_captcha_pair():
+    pass
--- server/api/settings.py
+++ server/api/settings.py
@@ -131,3 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "/api/static")
+
+# Seed Data location
+
+SEED_DATA_PATH = BASE_DIR + '/../'
