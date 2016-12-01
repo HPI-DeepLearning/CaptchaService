11eb8efca91137a32668acedbc165dd075615427
slayerpart <marvin.gorecki@student.hpi.uni-potsdam.de>
2016-12-01 16:59:12 +0100

    add CaptchaToken constructor

--- server/api/captcha_service/models.py
+++ server/api/captcha_service/models.py
@@ -1,14 +1,29 @@
 from django.db import models
+from django.core.files.base import ContentFile
 from picklefield.fields import PickledObjectField
 from encrypted_fields import EncryptedCharField
+from collections import Counter
 
 class CaptchaToken(models.Model):
-    file = models.ImageField(upload_to='captchas')
+    file = models.ImageField(upload_to='static/captchas/')
     # counter object that counts user proposals
     # to this captcha. If captcha is solved None is saved.
     proposals = PickledObjectField()
     resolved = models.BooleanField(default=False)
     result = EncryptedCharField(max_length=256)
+
+    @classmethod
+    def create(cls, file_name, file_data, resolved, result=''):
+        token = cls()
+        token.file.save(file_name, ContentFile(file_data))
+        token.proposals = Counter()
+        token.resolved = resolved
+        token.result = result
+        return token
+
+    def add_proposal(self, proposal):
+        self.proposals['proposal'] += 1
+        self.save()
 
 class CaptchaSession(models.Model):
     session_key = models.CharField(primary_key=True, unique=True, max_length=256)
