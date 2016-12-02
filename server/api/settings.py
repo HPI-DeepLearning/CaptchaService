b7465a4aebcdbe10e6efea6e5b72aa09038defc4
slayerpart <marvin.gorecki@student.hpi.uni-potsdam.de>
2016-12-02 14:30:53 +0100

    serve static files properly

--- server/api/settings.py
+++ server/api/settings.py
@@ -132,7 +132,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "/api/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+print(STATIC_ROOT)
+
+
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'api', 'static/'),
+)
+
+STATICFILES_FINDERS = (
+  'django.contrib.staticfiles.finders.FileSystemFinder',
+  'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 # Seed Data location
 
