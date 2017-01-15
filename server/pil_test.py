736d9e4fd606b2c947dec93a80b98855effd01e3
hendraet <hendrik.raetz@niederjahna.de>
2017-01-15 16:51:22 +0100

    Added algorithm for finding dominant color with kmeans

--- server/pil_test.py
+++ server/pil_test.py
@@ -4,42 +4,20 @@ import sys
 import math
 import numpy as np
 import random
-from decimal import *
 
+from decimal import *
 from PIL import Image, ImageDraw
 
-# def shuffle(input_data, output_data, size):
-# 	width, height = size
-# 	pix=[0, 0]
-# 	delta_x = 40     #lower delta for high distortion
-# 	delta_y = 40     #higher delta for low distortion
-#
-##  	for x in range(width):
-# 		for y in range(height):
-# 			x_shift, y_shift =  ( int(abs(math.sin(x)*width/delta_x)) ,
-# 					int(abs(math.tan(math.sin(y)))*height/delta_y))
-#
-# 			if x + x_shift < width:
-# 				pix[0] = x + x_shift
-# 			else:
-# 				pix[0] = x
-#
-# 			if y + y_shift < height :
-# 				pix[1] = y + y_shift
-# 			else:
-# 				pix[1] = y
-#
-# 			output_data[x,y] = input_data[tuple(pix)]
+import scipy
+import scipy.misc
+import scipy.cluster
 
 def wave_transformation(input_data, output_data, width, height):
-	frequency_modifier = Decimal(random.uniform(0.6, 0.8) * (width / height))
+	frequency_modifier = Decimal(random.uniform(0.6, 0.8) * (width / height)) #slight randomisation for frequency
 	frequency = frequency_modifier * (Decimal(1) / Decimal((width / 2))) * Decimal(math.pi)
 
-	amplitude_modifier = random.uniform(5, 7)
+	amplitude_modifier = random.uniform(5, 7) #slight randomisation for amplitude
 	amplitude = (height / amplitude_modifier)
-
-#	print(width, height)
-#	print(frequency, amplitude)
 
 	for x in range(width):
 		for y in range(height):
@@ -54,9 +32,21 @@ def wave_transformation(input_data, output_data, width, height):
 				output_data[x,y] = input_data[x, (height-1)]
 
 def find_dominant_color(input_img):
-	number_of_pixels = input_img.size[0]*input_img.size[1]
-	color_list = input_img.getcolors(maxcolors = number_of_pixels)
-	return sorted(color_list, reverse=True)[0][1]
+	NUM_CLUSTERS = 5
+
+	image_array = scipy.misc.fromimage(input_img) #numpy array from input image
+	shape = image_array.shape
+	image_array = image_array.reshape(scipy.product(shape[:2]), shape[2])
+
+	codes, distortion = scipy.cluster.vq.kmeans(image_array.astype(float), NUM_CLUSTERS)
+
+	vecs, distortion = scipy.cluster.vq.vq(image_array, codes)
+	counts, bins = scipy.histogram(vecs, len(codes))
+
+	index_max = scipy.argmax(counts)
+	peak = codes[index_max].astype(int)
+
+	return tuple(peak)
 
 def main():
 	infile = sys.argv[1]
@@ -76,18 +66,14 @@ def main():
 	draw = ImageDraw.Draw(input_img)
 	draw.line((0, height/2, width-1, height/2), fill=dominant_color, width=height/10)
 
-	if sys.argv[2] == "w":
-		wave_transformation(input_data, output_data, width, height)
+	wave_transformation(input_data, output_data, width, height)
 
-	#if sys.argv[2] == "s":
-	#	shuffle(input_data, output_data, input_img.size)
-	#if sys.argv[2] == "b":
-	#	wave_transformation(input_data, output_data, input_img.size)
-	#	shuffle(output_data, output_data, input_img.size)
+	if len(sys.argv) > 2:
+		if sys.argv[2] == "show":
+			input_img.show()
+			output_img.show()
 
 	output_img.save(outfile)
-	input_img.show()
-	output_img.show()
 
 if __name__ == "__main__":
 	main()
