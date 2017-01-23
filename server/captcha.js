a57c28516449e25ef398cdcefbc58d36ce14e309
slayerpart <marvin.gorecki@student.hpi.uni-potsdam.de>
2017-01-23 16:25:28 +0100

    render text captcha properly - dynamic image sizing close #9

--- server/captcha.js
+++ server/captcha.js
@@ -22,7 +22,67 @@ var oldOverlay = '<div class="overlay"><div class="valign card blue-grey darken-
          '<a id="reload" href=""><i class="material-icons" style="font-size: 1.2rem">refresh</i></a>' +
        '</div>' +
 '</div></div>';
-var captchaURL = captchaURL || 'http://localhost:8000/captcha/request';
+
+var captchaSolved = false,
+    HTMLOverlay = {
+        text: '<div class="overlay">' +
+          '<div class="captcha-card">' +
+            '<div class="captcha-content">' +
+                '<div class="captcha-image-container">' +
+                    '<div class="first-captcha"><img id="first-text-token"></div>' +
+                    '<div class="second-captcha"><img id="second-text-token"></div></div>' +
+                '</div>' +
+                '<div class="captcha-input"><input type="text" placeholder="Answer..."></input></div>' +
+                '<div class="captcha-actions">' +
+                    '<a href="#" id="submit">Submit</a>' +
+                    '<a href="#" id="reload"><i class="fa fa-refresh">refresh</i></a>' +
+                '</div>' +
+            '</div>' +
+          '</div>' +
+        '</div>',
+        image: ''
+    },
+    insertCaptchaData = {
+        text: function (response) {
+            var firstImage = document.querySelectorAll('.first-captcha > img')[0],
+                secondImage = document.querySelectorAll('.second-captcha > img')[0];
+            if (!firstImage || !secondImage) {
+                throw new Error('missing text captcha markup!');
+            }
+            firstImage.setAttribute('src', baseURL + response.first_url);
+            secondImage.setAttribute('src', baseURL + response.second_url);
+            var setMaxImageDimensions = function (imageOne, imageTwo, fi_w, fi_h, si_w, si_h) {
+                var container = document.getElementsByClassName('captcha-image-container')[0],
+                    availableSpace = container.clientWidth,
+                    availableImageWidth = availableSpace - 36, // 8px border on image + 5 px outer paddding
+                    maxHeight = window.innerHeight * 0.4;
+                var fi_dimension = fi_w / fi_h,
+                    si_dimension = si_w / si_h,
+                    fi_resultingWidth = fi_dimension * availableImageWidth / (fi_dimension + si_dimension),
+                    si_resultingWidth = availableImageWidth - fi_resultingWidth;
+                console.log(availableImageWidth);
+                console.log(fi_resultingWidth);
+                console.log(si_resultingWidth);
+                imageOne.style.width = '' + fi_resultingWidth + 'px';
+                imageTwo.style.width = '' + si_resultingWidth + 'px';
+            }
+            secondImage.onload = function() {
+                var firstImageWidth  = firstImage.naturalWidth;
+                var firstImageHeight = firstImage.naturalHeight;
+                var secondImageWidth  = secondImage.naturalWidth;
+                var secondImageHeight = secondImage.naturalHeight;
+                setMaxImageDimensions(firstImage, secondImage, firstImageWidth,
+                                      firstImageHeight, secondImageWidth, secondImageHeight);
+            }
+        },
+        image: function(response) {
+            // TODO
+        }
+    }
+
+/* 1. Open captcha session */
+var baseURL = baseURL || 'http://localhost:8000/';
+var captchaURL = baseURL + 'captcha/request';
 var captchaReq = new XMLHttpRequest();
 var response = "";
 captchaReq.open('GET', captchaURL, true);
@@ -32,16 +92,69 @@ captchaReq.onload = function (e) {
     if(captchaReq.status === 200) {
         var xhr = e.target;
         if (xhr.responseType === 'json') {
-            response = xhr.response.message;
+            console.log(xhr);
+            response = xhr.response;
+            handleResponse(response);
         } else {
-            response = JSON.parse(xhr.responseText).message; // IE bug fix
+            response = JSON.parse(xhr.responseText); // IE bug fix
         }
     } else {
       throw new Error('An error occurred during your request: ' +  captchaReq.status + ' ' + captchaReq.statusText);
     }
 };
 captchaReq.send();
-console.log(response);
+
+var handleResponse = function (response) {
+    var type = response.type,
+        session_key = response.session_key;
+
+    /* 2. Append captcha DOM Node to body */
+    // cross browser trick (https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro)
+
+    var div = document.createElement('div');
+    div.innerHTML = HTMLOverlay[type];
+    var captchaOverlay = div.firstChild;
+    // Append overlay to body with rendered captcha (display: none)
+    document.body.appendChild(captchaOverlay);
+    // retrieve window size and append this size to overlay
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    captchaOverlay.style.height = h;
+    captchaOverlay.style.width = w;
+
+    insertCaptchaData[type](response);
+
+
+    /* 3. Lookup form and form button element */
+
+    var form = (!document.querySelectorAll('#captcha-form'))? false : document.querySelectorAll('#captcha-form')[0],
+        button = (!document.querySelectorAll('#captcha-button'))? false : document.querySelectorAll('#captcha-button')[0];
+    if (!button) {
+        throw new Error('form button element with id "captcha-button" does not exist');
+    }
+    if (!form) {
+        throw new Error('form element with id "captcha-form" does not exist');
+    }
+
+    /* 4. Add eventlistener on form button to summon overlay */
+
+    var showCaptchaOverlay = function () {
+        var body = document.querySelectorAll('body');
+
+    }
+    button.addEventListener('click', function(e) {
+        if(!captchaSolved) {
+            e.preventDefault();
+            // show captcha overlay
+            captchaOverlay.classList.add('show');
+            setTimeout(function(){captchaOverlay.classList.add('fadeIn');}, 10);
+        }
+    })
+}
+
+// 4. POST Form on validation and secrect key generated by Web Service
+
+
 /*
 $('#reload').on('click', function(e) {
     e.preventDefault();
@@ -84,60 +197,3 @@ $('#submit').on('click', function(e) {
     })
 });
 */
-
-var captchaSolved = false,
-    styledTextCaptchaOverlayHTML = '<div class="overlay">' +
-      '<div class="captcha-card">' +
-        '<div class="captcha-content">' +
-            '<div class="captcha-image-container">' +
-                '<div class="first-captcha"><img id="first-text-token"></div>' +
-                '<div class="secondcaptcha"><img id="second-text-token"></div></div>' +
-            '</div>' +
-            '<div class="captcha-input"></div>' +
-        '</div>' +
-        '<div class="captcha-actions">' +
-            '<a href="#" id="submit">Submit</a>' +
-            '<a href="#" id="reload"><i class="fa fa-refresh">refresh</i></a>' +
-        '</div>' +
-      '</div>' +
-    '</div>';
-
-// append captcha DOM Node to body
-// cross browser trick (https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro)
-var div = document.createElement('div');
-div.innerHTML = styledTextCaptchaOverlayHTML;
-var captchaOverlay = div.firstChild;
-// Append overlay to body with rendered captcha (display: none)
-document.body.appendChild(captchaOverlay);
-// retrieve window size and append this size to overlay
-var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-captchaOverlay.style.height = h;
-captchaOverlay.style.width = w;
-
-// Lookup form and form button element
-var form = (!document.querySelectorAll('#captcha-form'))? false : document.querySelectorAll('#captcha-form')[0],
-    button = (!document.querySelectorAll('#captcha-button'))? false : document.querySelectorAll('#captcha-button')[0];
-if (!button) {
-    throw new Error('form button element with id "captcha-button" does not exist');
-}
-if (!form) {
-    throw new Error('form element with id "captcha-form" does not exist');
-}
-
-// 3. Add eventlistener on form button to summon overlay
-var showCaptchaOverlay = function () {
-    var body = document.querySelectorAll('body');
-
-}
-button.addEventListener('click', function(e) {
-    if(!captchaSolved) {
-        e.preventDefault();
-        // show captcha overlay
-        captchaOverlay.classList.add('show');
-        setTimeout(function(){captchaOverlay.classList.add('fadeIn');}, 10);
-    }
-})
-
-
-// 4. POST Form on validation and secrect key generated by Web Service
