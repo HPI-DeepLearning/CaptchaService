aeae910946a6761103e6ee45cc6501711e5ebb5a
git <robert.stark@student.hpi.uni-potsdam.de>
2017-01-30 17:20:57 +0100

    ignore

--- server/api/captcha_service/urls.py
+++ server/api/captcha_service/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     url(r'^request$', views.request, name="request"),
     url(r'^validate$', views.validate, name="validate"),
     url(r'^renew$', views.renew, name="renew"),
-    url(r'upload$', views.upload, name="upload"),
+    url(r'^upload$', views.upload, name="upload"),
 ]
