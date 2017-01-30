69f8f939c45e644f7980b49fba55e73858d1be2f
slayerpart <marvin.gorecki@student.hpi.uni-potsdam.de>
2017-01-30 21:50:04 +0100

    fix internal server error caused by missing registration routes

--- server/api/urls.py
+++ server/api/urls.py
@@ -21,7 +21,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^upload/', TemplateView.as_view(template_name='upload.html'), name='upload'),
-    url(r'^accounts/', include('registration.urls')),
+    #url(r'^accounts/', include('registration.urls')), TODO: does not exist yet !?
     url(r'^captcha/', include('captcha_service.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
