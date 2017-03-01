de6e0935d6a3824316f8ae6af06913fceb059735
Christian Bartz <christian.bartz@student.hpi.uni-potsdam.de>
2017-03-01 17:21:09 +0100

    show custom models in django admin

--- server/api/captcha_service/admin.py
+++ server/api/captcha_service/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import CaptchaToken, TextCaptchaToken, ImageCaptchaToken
+
 # Register your models here.
+
+admin.site.register(CaptchaToken)
+admin.site.register(TextCaptchaToken)
+admin.site.register(ImageCaptchaToken)
